d1619a82cfe3eb4ae0ac7bcbc463656d505b5c61
TheConnMan <bcconn2112@gmail.com>
2016-10-15 12:56:29 -0400

    Add environment variable support for OAuth keys

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-  serverUrl: 'http://127.0.0.1:1337'
+  serverUrl: process.env.SERVER_URL || 'http://127.0.0.1:1337'
 
 };
--- config/env/production.js
+++ config/env/production.js
@@ -1,38 +1,21 @@
-/**
- * Production environment settings
- *
- * This file can include shared settings for a production environment,
- * such as API keys or remote database passwords.  If you're using
- * a version control solution for your Sails app, this file will
- * be committed to your repository unless you add it to your .gitignore
- * file.  If your repository will be publicly viewable, don't add
- * any private information to this file!
- *
- */
-
 module.exports = {
 
-  /***************************************************************************
-   * Set the default database connection for models in the production        *
-   * environment (see config/connections.js and config/models.js )           *
-   ***************************************************************************/
+  port: 80,
+  serverUrl: process.env.SERVER_URL || 'http://127.0.0.1',
 
-  // models: {
-  //   connection: 'someMysqlServer'
-  // },
-
-  /***************************************************************************
-   * Set the port in the production environment to 80                        *
-   ***************************************************************************/
-
-  // port: 80,
-
-  /***************************************************************************
-   * Set the log level in production environment to "silent"                 *
-   ***************************************************************************/
-
-  // log: {
-  //   level: "silent"
-  // }
+  oauth: {
+    github: {
+      clientID: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
+    },
+    google: {
+      clientID: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    },
+    twitter: {
+      consumerKey: process.env.TWITTER_KEY,
+      consumerSecret: process.env.TWITTER_SECRET
+    }
+  }
 
 };
