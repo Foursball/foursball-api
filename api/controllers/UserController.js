3231e5539e86066683ec7ff7bd9ea47bfe5754d7
sirzach <zach.mcgonigal@gmail.com>
2017-02-27 19:22:29 -0500

    adding a current user endpoint and check for proxy in the isAuthenticated policy

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,3 +1,14 @@
-module.exports = {
+const log4js = require('log4js');
+const logger = log4js.getLogger('config/bootstrap');
 
+module.exports = {
+  current(req, res) {
+    User.findOne({ id: req.session.passport.user }, (err, user) => {
+      if (!err) {
+        res.json({ user });
+      } else {
+        res.status(404);
+      }
+    });
+  }
 };
