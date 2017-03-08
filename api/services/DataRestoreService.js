f1602bfccaa9ce4f6866b91c2af92a21ee9d7c76
sirzach <zach.mcgonigal@gmail.com>
2017-03-08 08:31:10 -0500

    continuing working on the data restore service

--- api/services/DataRestoreService.js
+++ api/services/DataRestoreService.js
@@ -11,7 +11,23 @@ module.exports = {
 };
 
 function restore() {
+  let LeaguesJson = require(path.join(cwd, 'restore', 'leagues.json'));
   let FoosballersJson = require(path.join(cwd, 'restore', 'foosballers.json'));
+  let GamesJson = require(path.join(cwd, 'restore', 'games.json'));
+  let RulesJson = require(path.join(cwd, 'restore', 'rules.json'));
+  let TeamsJson = require(path.join(cwd, 'restore', 'teams.json'));
+  let NetuitiveLeage = LeagueJson.netuitive;
+  let leaguePromises = Object.keys(LeagueJson).map((l) => {
+    let L = LeaguesJson[l];
+
+    return League.create({
+      id: l,
+      name: L.name,
+      seasons: Object.keys(L.seasons),
+      rules: Object.keys(rules)
+    });
+  });
+
   let userPromises = Object.keys(FoosballersJson).map((f) => {
     let U = FoosballersJson[f];
 
@@ -22,19 +38,41 @@ function restore() {
       retired: U.retired,
       isAdmin: U.isAdmin ? true : false,
       isGlobalAdmin: U.isGlobalAdmin ? true : false,
-      profileImageURL: U.profileImageURL
+      profileImageURL: U.profileImageURL,
+      id: f
     });
   });
 
-  let GamesJson = require(path.join(cwd, 'restore', 'games.json'));
   let gamePromises = Object.keys(GamesJson).map((g) => {
     let G = GamesJson[g];
 
     return Game.create({
       dateCreated: G.time,
-      teamGames: [{
-
-      }];
+      id: g
     });
+  });
+
+  let rulesPromises = Object.keys(RulesJson).map((r) => {
+    let R = RulesJson[r];
+
+    return Rule.create({
+      id: r,
+      dateCreated: R.date,
+      message: R.message,
+      league: R.tenant
+    });
+  });
+
+  let teamsPromises = Object.keys(TeamsJson).map((t) => {
+    let T = TeamsJson[t];
+
+    return Team.create({
+      id: t,
+      users: [
+        T.player1, T.player2
+      ]
+    });
+
+    // let teamGamesPromises = 
   });
 }
