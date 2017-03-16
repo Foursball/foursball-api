35fc09b1939b314b03b095a8d34c584547ceb879
sirzach <zach.mcgonigal@gmail.com>
2017-03-16 18:03:35 -0400

    setting up the models for uuids

--- api/models/League.js
+++ api/models/League.js
@@ -1,6 +1,17 @@
+const uuid = require('uuid');
+
 module.exports = {
+  autoPK: false,
 
   attributes: {
+    id: {
+      type: 'string',
+      primaryKey: true,
+      unique: true,
+      index: true,
+      uuidv4: true,
+      defaultsTo: function (){ return uuid.v4(); },
+    },
     name: { type: 'string' },
     minimumGames: { type: 'integer' },
 
